03f0d2534e63abcb831d7d1d184782a96d5509af
Anshuman Bhardwaj <anshu5074@gmail.com>
2020-06-18 10:13:22 +0530

    Refactor App props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Provider} from 'react-redux';
@@ -14,39 +14,40 @@ const home_active = require('./assets/home_active.png');
 const heart_active = require('./assets/heart_active.png');
 const heart_inactive = require('./assets/heart_inactive.png');
 
+const tabBarOptions = {
+  activeTintColor: theme.primary,
+  inactiveTintColor: theme.secondary,
+  labelStyle: {
+    fontSize: 14,
+  },
+  keyboardHidesTabBar: true,
+};
+
+function getScreenOptions({route}) {
+  return {
+    tabBarIcon: ({focused, color, size}) => {
+      let icon;
+      if (route.name === 'Home') {
+        icon = focused ? home_active : home_inactive;
+      } else if (route.name === 'Favourites') {
+        icon = focused ? heart_active : heart_inactive;
+      }
+      return <Image source={icon} style={{width: 20, height: 20}} />;
+    },
+  };
+}
+
 export default function App() {
   return (
-    <View style={{flex: 1, backgroundColor: theme.gray}}>
-      <NavigationContainer>
-        <Provider store={store}>
-          <Tab.Navigator
-            screenOptions={({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                let icon;
-                if (route.name === 'Home') {
-                  icon = focused ? home_active : home_inactive;
-                } else if (route.name === 'Favourites') {
-                  icon = focused ? heart_active : heart_inactive;
-                }
-                // You can return any component that you like here!
-                return (
-                  <Image source={icon} style={{width: 20, height: 20}} />
-                );
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: theme.primary,
-              inactiveTintColor: theme.secondary,
-              labelStyle: {
-                fontSize: 14,
-              },
-              keyboardHidesTabBar: true,
-            }}>
-            <Tab.Screen name="Home" component={SearchScreen} />
-            <Tab.Screen name="Favourites" component={FavouritesScreen} />
-          </Tab.Navigator>
-        </Provider>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Provider store={store}>
+        <Tab.Navigator
+          screenOptions={getScreenOptions}
+          tabBarOptions={tabBarOptions}>
+          <Tab.Screen name="Home" component={SearchScreen} />
+          <Tab.Screen name="Favourites" component={FavouritesScreen} />
+        </Tab.Navigator>
+      </Provider>
+    </NavigationContainer>
   );
 }
